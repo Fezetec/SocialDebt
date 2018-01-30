060e29c1bde44bc2872fff6cde4390b021ae5e90
Fezetec <vinjarkvamme@gmail.com>
2018-01-30 15:38:19 +0100

    Replace DarkActionBar with Toolbar in MainActivity

--- app/src/main/java/drunkendevs/socialdebt/MainActivity.java
+++ app/src/main/java/drunkendevs/socialdebt/MainActivity.java
@@ -2,6 +2,7 @@ package drunkendevs.socialdebt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar); // get the reference of Toolbar
+        setSupportActionBar(toolbar); // Setting/replace toolbar as the ActionBar
     }
 }
