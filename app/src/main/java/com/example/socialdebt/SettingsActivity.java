e1cd2bd2715fd6aab125e6eff2245bd2a850bbce
Vinjar Nesse Kvamme <vinjar@acos.no>
2022-03-01 16:06:50 +0100

    Legger til touchListener på sliders

--- app/src/main/java/com/example/socialdebt/SettingsActivity.java
+++ app/src/main/java/com/example/socialdebt/SettingsActivity.java
@@ -1,34 +1,40 @@
 package com.example.socialdebt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.slider.Slider;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener, Slider.OnSliderTouchListener {
+    // View Elements
+    Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ListActivities();
 
-        ListOutActivities();
+        btnSave = findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(this);
     }
     
-    private void ListOutActivities() {
+    private void ListActivities() {
         View llLayout = findViewById(R.id.llLayout);
         for (Activity act : MainActivity.activities) {
             TextView txtView = new TextView(this);
             txtView.setText(act.getName());
             txtView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-            //RelativeLayout.LayoutParams lParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-            //lParams.addRule(RelativeLayout.BELOW, txtView.getId());
-            //txtView.setLayoutParams(lParams);
             ((LinearLayout) llLayout).addView(txtView);
 
             Slider slider = new Slider(this);
@@ -37,7 +43,34 @@ public class SettingsActivity extends AppCompatActivity {
             slider.setValue(act.getPoints());
             slider.setStepSize(1);
             slider.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            slider.addOnSliderTouchListener(this);
             ((LinearLayout) llLayout).addView(slider);
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnSave:
+                // Save
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.putExtra("activities", MainActivity.activities);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public void onStartTrackingTouch(@NonNull Slider slider) {
+
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public void onStopTrackingTouch(@NonNull Slider slider) {
+        slider.getValue();
+        // TODO Lagre slider.value til MainActivity.activities
     }
 }
