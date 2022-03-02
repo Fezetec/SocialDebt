9451125967a3fd03313d27e780258c8e285a73d0
Vinjar Nesse Kvamme <vinjar@acos.no>
2022-03-02 15:09:51 +0100

    Ny dialogaktivitet for å legge til nye. Kan slette aktiviteter Fjerner titlebar Scrollbar i settings

---
+++ app/src/main/java/com/example/socialdebt/NewActivityDialog.java
@@ -0,0 +1,58 @@
+package com.example.socialdebt;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+import com.google.android.material.slider.Slider;
+
+import java.util.ArrayList;
+
+public class NewActivityDialog extends AppCompatDialogFragment {
+
+    private NewActivityDialogListener listener;
+    private EditText txtName;
+    private Slider sldScore;
+
+    @Override
+    public android.app.Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_dialog_activity, null);
+
+        builder.setView(view)
+                .setNegativeButton("Cancel", (dialogInterface, i) -> {
+
+                })
+                .setPositiveButton("OK", (dialogInterface, i) -> {
+                    Activity act = new Activity(txtName.getText().toString(), (int) sldScore.getValue());
+                    //int score = act.getPoints();
+                    listener.addActivity(act);
+                });
+        txtName = view.findViewById(R.id.txtName);
+        sldScore = view.findViewById(R.id.sldScore);
+        return builder.create();
+    }
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        try {
+            listener = (NewActivityDialog.NewActivityDialogListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + "must implement NewActivityDialogListener");
+        }
+    }
+
+    public interface NewActivityDialogListener {
+        void addActivity(Activity act);
+    }
+}
--- app/src/main/java/com/example/socialdebt/SettingsActivity.java
+++ app/src/main/java/com/example/socialdebt/SettingsActivity.java
@@ -7,19 +7,22 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.slider.Slider;
 import com.google.gson.Gson;
 
-public class SettingsActivity extends AppCompatActivity implements View.OnClickListener, Slider.OnSliderTouchListener {
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener, Slider.OnSliderTouchListener, NewActivityDialog.NewActivityDialogListener {
     // View Elements
     Button btnSave;
+    Button btnNew;
+    View llLayout;
     Gson gson;
 
     @Override
@@ -32,16 +35,38 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
 
         btnSave = findViewById(R.id.btnSave);
         btnSave.setOnClickListener(this);
+        btnNew = findViewById(R.id.btnNew);
+        btnNew.setOnClickListener(this);
     }
     
     private void ListActivities() {
-        View llLayout = findViewById(R.id.llLayout);
+        llLayout = findViewById(R.id.llLayout);
+        ((LinearLayout) llLayout).removeAllViews();
         int counter = 0;
         for (Activity act : MainActivity.activities) {
             TextView txtView = new TextView(this);
             txtView.setText(act.getName());
             txtView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            txtView.setPadding(30, 0, 30, 0);
             ((LinearLayout) llLayout).addView(txtView);
+
+            ImageView img = new ImageView(this);
+            img.setId(counter);
+            img.setImageDrawable(getResources().getDrawable(R.drawable.ic_launcher_background));
+            img.setLayoutParams(new LinearLayout.LayoutParams(100, 100));
+            img.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    MainActivity.activities.remove(view.getId());
+                    String jsonSetActivities = gson.toJson(MainActivity.activities);
+                    SharedPreferences.Editor editor = MainActivity.spActivities.edit();
+                    editor.putString("activities", jsonSetActivities);
+                    editor.commit();
+                    Toast.makeText(SettingsActivity.this, "Deleted!", Toast.LENGTH_SHORT).show();
+                    ListActivities();
+                }
+            });
+            ((LinearLayout) llLayout).addView(img);
 
             Slider slider = new Slider(this);
             slider.setId(counter);
@@ -49,7 +74,9 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
             slider.setValueTo(10);
             slider.setValue(act.getPoints());
             slider.setStepSize(1);
-            slider.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.gravity = Gravity.BOTTOM;
+            slider.setLayoutParams(params);
             slider.addOnSliderTouchListener(this);
             ((LinearLayout) llLayout).addView(slider);
             counter++;
@@ -60,20 +87,26 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.btnSave:
+                // Save
                 String jsonSetActivities = gson.toJson(MainActivity.activities);
                 SharedPreferences.Editor editor = MainActivity.spActivities.edit();
                 editor.putString("activities", jsonSetActivities);
                 editor.commit();
-                Toast.makeText(SettingsActivity.this, "Saved!", Toast.LENGTH_SHORT);
+                Toast.makeText(SettingsActivity.this, "Saved!", Toast.LENGTH_SHORT).show();
 
-                // Save
+                //Redirect to MainActivity
                 Intent intent = new Intent(this, MainActivity.class);
-                intent.putExtra("activities", MainActivity.activities);
+                startActivity(intent);
+                break;
+            case R.id.btnNew:
+                openNewActivityDialog();
                 break;
             default:
                 break;
         }
     }
+
+    // TODO OnEditListener på EditText i onCreate
 
     @SuppressLint("RestrictedApi")
     @Override
@@ -86,6 +119,20 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
     public void onStopTrackingTouch(@NonNull Slider slider) {
         // TODO Lagre slider.value til MainActivity.activities
         MainActivity.activities.get(slider.getId()).setPoints((int) slider.getValue());
+    }
 
+    @Override
+    public void addActivity(Activity act) {
+        MainActivity.activities.add(act);
+        String jsonSetActivities = gson.toJson(MainActivity.activities);
+        SharedPreferences.Editor editor = MainActivity.spActivities.edit();
+        editor.putString("activities", jsonSetActivities);
+        editor.commit();
+        ListActivities();
+    }
+
+    private void openNewActivityDialog() {
+        NewActivityDialog newActivityDialog = new NewActivityDialog();
+        newActivityDialog.show(getSupportFragmentManager(), "ADD ACTIVITY");
     }
 }
